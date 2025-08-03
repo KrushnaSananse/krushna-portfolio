072982a925492c643a212bf265a1849be6a23575
KrushnaSananse <132080099+KrushnaSananse@users.noreply.github.com>
2025-08-03 23:47:32 +0530

    Eddited content to center according to mobile view

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,13 +15,13 @@ const Hero = () => {
             <div className='w-full'>
                 <div className='flex flex-col items-center'>
                     <h1 className='pb-16 text-3xl font-thin tracking-tight lg:mt-16 lg:text-7xl'>Krushna Sananse</h1>
-                    <span className='bg-gradient-to-r
+                    <span className='text-center  bg-gradient-to-r
                          from-pink-300 via-slate-500
                          to-purple-500 bg-clip-text 
                          text-3xl lg:text-4xl tracking-tight text-transparent '>
                         Frontend | Backend | Full Stack Developer
                     </span>
-                    <p className='mt-10 min-w-xl py-6 font-light tracking-tighter lg:text-xl text-neutral-500'>
+                    <p className='text-center mt-10 min-w-xl py-6 font-light tracking-tighter lg:text-xl text-neutral-500'>
                         {HERO_CONTENT}
                     </p>
                    <div className="flex flex-col items-center justify-center gap-6 pb-10 lg:flex-row lg:items-start">
